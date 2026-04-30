dbc87a0f08a06631d23fba2e29617966c0684521
Bahnishikha Singha <aniopt06@gmail.com>
2026-05-01 00:19:02 +0530

    move the homepage into react

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,56 +1,42 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Ecommerce Project</title>
+import './header.css';
+import './Homepage.css';
 
-    <!-- This code is needed for responsive design to work.
-      (Responsive design = make the website look good on
-      smaller screen sizes like a phone or a tablet). -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Load a font called Roboto from Google Fonts. -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-
-    <!-- Here are the CSS files for this page. -->
-    <link rel="stylesheet" href="styles/shared/general.css">
-    <link rel="stylesheet" href="styles/shared/header.css">
-    <link rel="stylesheet" href="styles/pages/index.css">
-  </head>
-  <body>
-    <div class="header">
-      <div class="left-section">
-        <a href="index.html" class="header-link">
-          <img class="logo"
+export function Homepage() {
+  return (
+    <>
+    <div className="header">
+      <div className="left-section">
+        <a href="index.html" className="header-link">
+          <img className="logo"
             src="images/logo-white.png" />
-          <img class="mobile-logo"
+          <img className="mobile-logo"
             src="images/mobile-logo-white.png" />
         </a>
       </div>
 
-      <div class="middle-section">
-        <input class="search-bar" type="text" placeholder="Search" />
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
 
-        <button class="search-button">
-          <img class="search-icon" src="images/icons/search-icon.png" />
+        <button className="search-button">
+          <img className="search-icon" src="images/icons/search-icon.png" />
         </button>
       </div>
 
-      <div class="right-section">
-        <a class="orders-link header-link" href="orders.html">
+      <div className="right-section">
+        <a className="orders-link header-link" href="orders.html">
 
-          <span class="orders-text">Orders</span>
+          <span className="orders-text">Orders</span>
         </a>
 
-        <a class="cart-link header-link" href="checkout.html">
-          <img class="cart-icon" src="images/icons/cart-icon.png" />
-          <div class="cart-quantity">3</div>
-          <div class="cart-text">Cart</div>
+        <a className="cart-link header-link" href="checkout.html">
+          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <div className="cart-quantity">3</div>
+          <div className="cart-text">Cart</div>
         </a>
       </div>
     </div>
 
+    
     <div class="home-page">
       <div class="products-grid">
         <div class="product-container">
@@ -201,5 +187,6 @@
         </div>
       </div>
     </div>
-  </body>
-</html>
+    </>
+  );
+}
