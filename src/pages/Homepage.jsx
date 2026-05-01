f040a0c42beb87d221473fb3e31d9bfbb87fc84c
Bahnishikha Singha <aniopt06@gmail.com>
2026-05-01 20:01:59 +0530

    fetch data from the backend

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,14 @@ import { products } from '../../starting-code/data/products';
 import './Homepage.css';
 
 export function Homepage() {
+
+  fetch('http://localhost:3000/api/products')
+   .then((response) => {
+    return response.json();
+   }).then((data) => {
+     console.log(data);
+   });
+
   return (
     <>
       <Header />
